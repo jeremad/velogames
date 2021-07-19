2b9715597f6eb6cb2cde61a14155e5ce142e57db
jerem <jeremy.tellaa@tanker.io>
2021-07-19 10:41:23 +0200

    feat: allow a path argument

--- velogames/main.py
+++ velogames/main.py
@@ -1,10 +1,22 @@
+import argparse
+from pathlib import Path
+import shutil
+
 from velogames.computer import Computer
 from velogames.scrapper import scrap, CSV
 
 
 def main():
-    if not CSV.exists():
-        scrap()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csv", required=False)
+    args = parser.parse_args()
+
+    if args.csv:
+        f = Path(args.csv)
+        shutil.copy(f, CSV)
+    else:
+        if not CSV.exists():
+            scrap()
     computer = Computer()
     computer.compute()
     computer.publish()
