964c5a9643b44340d9ed3fa170aaf5765f27fbb2
jerem <jeremy.tellaa@tanker.io>
2021-07-20 21:18:01 +0200

    feat: make the type of contest configurable
    We only support grand tour and weekly races at the moment

--- velogames/rider.py
+++ velogames/rider.py
@@ -1,13 +1,28 @@
+from typing import Optional
+
+
 class Rider:
-    def __init__(self, name: str, team: str, rclass: str, score: int, cost: int):
+    def __init__(
+        self, name: str, team: str, score: int, cost: int, rclass: Optional[str] = None
+    ):
         self.name = name
         self.team = team
-        self.rclass = rclass
         self.score = score
         self.cost = cost
+        self.rclass = rclass
 
     @property
     def csv(self):
-        return ",".join(
-            [self.name, self.team, str(self.rclass), str(self.score), str(self.cost)]
-        )
+        if self.rclass is not None:
+            res = ",".join(
+                [
+                    self.name,
+                    self.team,
+                    str(self.rclass),
+                    str(self.score),
+                    str(self.cost),
+                ]
+            )
+        else:
+            res = ",".join([self.name, self.team, str(self.score), str(self.cost)])
+        return res
